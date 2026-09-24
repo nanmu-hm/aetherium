94c4cb75674927b855b343f2ce609d59f43b0ce2
nanmu-hm <liangzhongying74@gmail.com>
2026-09-24 19:22:13 +0700

    feat: export story emergence components

--- engine/narrative/__init__.py
+++ engine/narrative/__init__.py
@@ -1,1 +1,6 @@
 """Narrative observation and story-emergence components."""
+
+from .archaeologist import StoryArchaeologist, StoryCandidate
+from .pressure import NarrativePressureAnalyzer
+
+__all__ = ["NarrativePressureAnalyzer", "StoryArchaeologist", "StoryCandidate"]
